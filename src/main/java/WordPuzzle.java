8aecab18a93df81684ce2997adf56bfd73c6849c
farnoosh <farnoosh@pdx.edu>
2016-04-22 13:38:59 -0700

    WordPuzzle.java indentation finilized

--- src/main/java/WordPuzzle.java
+++ src/main/java/WordPuzzle.java
@@ -4,21 +4,13 @@ import java.util.HashMap;
 public class WordPuzzle {
 
   public static String runWordPuzzle(String word){
-    // String newWord = new String(word);
-    // newWord=word.toLowerCase(); //tEst -> test
 
-    // String result = newWord.replace('e','-'); //"t-st"
-    char[] myWordChars = word.toCharArray(); //test -> [t,E,s,t]
-    // String ifIsVowel = myWordChars.toString();
-    // String methodResult = "";
+    char[] myWordChars = word.toCharArray(); //for example: test -> [t,E,s,t]
 
     for(char vowel : myWordChars){
       //vowel case sensitivity
       if (vowel == 'a' || vowel == 'A'|| vowel == 'e'|| vowel == 'E'|| vowel == 'i' || vowel == 'I'|| vowel == 'o'|| vowel == 'O'|| vowel == 'u'|| vowel == 'U'){
         word = word.replaceAll("[aeiouAEIOU]","-");
-
-      // }else {
-      //   word=word;
       }
     } //end of for loop
 
