1acf8ccd49d7528053f15b5c4ccd5aad152ebf22
farnoosh johnson <farnoosh@pdx.edu>
2016-04-26 16:35:10 -0700

    remove the for and if statement in wordpuzzle.java file

--- src/main/java/WordPuzzle.java
+++ src/main/java/WordPuzzle.java
@@ -4,14 +4,7 @@ public class WordPuzzle {
 
   public static String runWordPuzzle(String word){
 
-    char[] myWordChars = word.toCharArray(); //for example: test -> [t,E,s,t]
-
-    for(char vowel : myWordChars){
-      //vowel case sensitivity
-      if (vowel == 'a' || vowel == 'A'|| vowel == 'e'|| vowel == 'E'|| vowel == 'i' || vowel == 'I'|| vowel == 'o'|| vowel == 'O'|| vowel == 'u'|| vowel == 'U'){
         word = word.replaceAll("[aeiouAEIOU]","-");
-      }
-    } //end of for loop
 
     return word;
   }
